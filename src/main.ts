965622e1b71c16fdf24d60dc696b8bd0576c6a40
Enrique Casanova <enrique.casanova@projekt202.com>
2022-03-18 15:29:04 -0500

    Add swagger persistAuthorization

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { NestFactory } from "@nestjs/core";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { TypeORMExceptionFilter } from "./utils/typeorm-exceptions.filter";
 import * as bodyParser from "body-parser";
@@ -15,8 +19,14 @@ async function bootstrap() {
     .build();
   app.use(bodyParser.json({ limit: "50mb" }));
   app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customSiteTitle: "ADK/p202 Superhero API",
+  };
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup("api", app, document);
+  SwaggerModule.setup("api", app, document, customOptions);
   await app.listen(process.env.port || 3000);
 }
 bootstrap();
