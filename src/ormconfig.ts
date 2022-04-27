830ad453f2210798aef0608c48c51c68376fdcd8
Enrique Casanova <ecasanova@webfactorystudio.co>
2022-04-27 12:05:27 -0500

    Change mysql to postgres

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,7 +2,7 @@ import { ConfigModule } from "@nestjs/config";
 ConfigModule.forRoot();
 
 const config: any = {
-  type: "mysql",
+  type: "postgres",
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USER,
