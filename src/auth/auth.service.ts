d0cecbc3692c754185d4fb92d417a9a358690d13
Enrique Casanova <ecasanova@webfactorystudio.co>
2022-04-27 19:13:06 -0500

    Change apiKey to private

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@nestjs/common";
 export class AuthService {
   // KEYS
   private apiKeys: string[] = [
-    "6b9a7f25-8ce5-4781-b1e1-98c40b1b3884", // ADK SUPERHERO SHARED API KEY
+    process.env.API_KEY, // ADK SUPERHERO PRIVATE API KEY
   ];
   validateApiKey(apiKey: string) {
     return this.apiKeys.find((apiK) => apiKey === apiK);
