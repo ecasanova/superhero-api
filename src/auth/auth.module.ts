7d7047731c583bbe7f260800b1e7df34af87f50a
Enrique Casanova <ecasanova@webfactorystudio.co>
2022-11-25 16:53:10 -0500

    Remove api auth strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,8 @@
 import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { PassportModule } from "@nestjs/passport";
-import { ApiKeyStrategy } from "./apiKey.strategy";
 @Module({
   imports: [PassportModule],
-  providers: [AuthService, ApiKeyStrategy],
+  providers: [AuthService],
 })
 export class AuthModule {}
